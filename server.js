2b67fe442f44d5fdf82a4fb1a1b74b19d5d9f2f0
Joe <lizhou1021@qq.com>
2022-01-29 14:12:42 +0800

    服务端加密

--- server.js
+++ server.js
@@ -7,11 +7,42 @@ const config = require("./webpack.config.js");
 const compiler = webpack(config);
 const PORT = 3000
 
+const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path")
+
+
+const key = Buffer.from(
+    "6b65796b65796b65796b65796b65796b65796b65796b6579",
+    "hex"
+).toString("utf8");
+
+app.get("/*.glb$/", function (req, res) {
+    console.log("123");
+    //先固定路径为例
+    const filepath = path.join(__dirname, "./public/models/Xbot.glb");
+    console.log("filepath: ", filepath);
+    if (!fs.existsSync(filepath)) {
+        res.status(404).send("");
+    } else {
+        const cipher = crypto.createCipheriv(
+            "aes-192-ctr",
+            key,
+            Buffer.alloc(16, 0)
+        );
+        const buf = Buffer.from(filepath);
+
+        fs.createReadStream(buf).pipe(cipher).pipe(res);
+    }
+});
+
 app.use(
     webpackDevMiddleware(compiler, {
         publicPath: config.output.publicPath,
     })
 );
+
+
 
 // app.use(express.static("./static"));
 
